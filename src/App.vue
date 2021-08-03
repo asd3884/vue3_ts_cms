92a16bed9c1181a17582ad1cbe217d8ee66f46d5
凌鑫余 <13207918861@163.com>
2021-08-03 23:32:09 +0800

    test(app test): app test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    哈哈
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
